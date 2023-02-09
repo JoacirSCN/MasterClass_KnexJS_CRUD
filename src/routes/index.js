09a49728693f6994ae59da8bd4cbeea51a7c0c88
joacir <joacirprog@gmail.com>
2023-02-09 10:29:18 -0300

    ...

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,7 +3,7 @@ const { Router } = require('express');
 const usersRoutes = require('./users.routes');
 const notesRoutes = require('./notes.routes');
 const tagsRoutes = require('./tags.routes');
-const sessionsRoutes = require('./sessions.Routes');
+const sessionsRoutes = require('./sessions.routes');
 
 
 const routes = Router();
